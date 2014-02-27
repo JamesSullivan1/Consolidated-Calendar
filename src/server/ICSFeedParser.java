9da3bbd46694a6b7fc306e746149b2dddd7a70c6
mike <m_widomski@yahoo.ca>
2014-02-27 12:14:55 -0700

    Parser layout change (not complete)

--- src/server/ICSFeedParser.java
+++ src/server/ICSFeedParser.java
@@ -1,4 +1,5 @@
 package server;
+import java.awt.Event;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -6,40 +7,56 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public final class ICSFeedParser {
-	public static ArrayList<String> ParseFeed(String link) throws FileNotFoundException{
-		File feed = new File(link);
+
+	public static String[] getCalendarData(File feed) throws FileNotFoundException {
 		Scanner parser = new Scanner(feed);
 		parser.useDelimiter(Pattern.compile("\\n"));
 		String current = null;
 		String servid = null;
 		String calname = null;
 		String[] splitter = null;
-		String eventData[] = new String[4];
-		ArrayList<String> parseData = new ArrayList<String>();
-		boolean flag = false;
-		
-		while(parser.hasNext()){
+		String[] calendarData = new String[2];
+		boolean flag = true;
+
+		while(parser.hasNext() && flag){
 			current = parser.next();
 			splitter = current.split(":");
 
 			if(splitter[0].equals("PRODID") && servid == null){
 				servid = current.split(":")[1];
-				parseData.add(servid);
+				calendarData[0] = servid;
 			} 
 			else if(splitter[0].equals("X-WR-CALNAME") && calname == null){
 				calname = current.split(":")[1];
-				parseData.add(calname);
+				calendarData[1] = calname;
+				flag = false;
 			}
+		}
+		parser.close();
+		return calendarData;
+	}
+
+	public static Event[] getEvents(File feed) throws FileNotFoundException {
+		Scanner parser = new Scanner(feed);
+		parser.useDelimiter(Pattern.compile("\\n"));
+		String current = null;
+		String[] splitter = null;
+		String[] eventData = new String[4];
+		ArrayList<Event> eventList = new ArrayList<Event>();
+		boolean flag = true;
+
+		while(parser.hasNext() && flag){
+			current = parser.next();
 
 			if(current.equals("BEGIN:VEVENT\r")){
 				flag = true;
 			}
-			
+
 			while(flag == true){
 
 				current = parser.next();
 				splitter = current.split(":");
-				
+
 				if(splitter[0].equals("SUMMARY")){
 					eventData[0] = splitter[1];
 				}else if(splitter[0].equals("LOCATION")){
@@ -50,22 +67,14 @@ public final class ICSFeedParser {
 					eventData[3] = splitter[1];
 				}else if(current.equals("END:VEVENT\r")){
 					flag = false;
-					for (int i = 0; i < eventData.length; i++) {
-						parseData.add(eventData[i]);
-					}
+					//need to parse string into dates
+					//need to pass calendar owner
+					Event e = new Event(eventData[0], eventData[1], eventData[2], eventData[3]);
+					eventList.add(e);
 				}
-				
+
 			}
 		}
-		parser.close();
-		return parseData;
 	}
-	
-//	public ArrayList<String> getCalendarData(ArrayList<String> rawData) {
-//		
-//	}
-//	
-//	public ArrayList<Event> getEvents(ArrayList<String> rawData) {
-//		
-//	}
 }
+
