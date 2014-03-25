c86f7530ce29d6396016d5050ee05500cbdd8a67
James.Sullivan1 <calgaryjames@gmail.com>
2014-03-25 11:19:01 -0600

    Added test package

---
+++ src/test/TestCalendar.java
@@ -0,0 +1,95 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCalendar {
+
+	public void testCalendarBuilder_goodInput() {
+		
+	}
+	
+	public void testCalendarBuilder_badInput() {
+		
+	}
+	
+	public void testCalendarBuilder_withService_goodInput() {
+		
+	}
+	
+	public void testCalendarBuilder_withService_badInput() {
+		
+	}
+	
+	public void testCalendarBuilder_withEmptyEvents() {
+		
+	}
+	
+	public void testCalendarBuilder_withEvents() {
+		
+	}
+	
+	public void testAddEvent() {
+		
+	}
+	
+	public void testAddRedundantEvent() {
+		
+	}
+	
+	public void testRemoveEvent_inEvents() {
+		
+	}
+	
+	public void testRemoveEvent_notInEvents() {
+		
+	}
+	
+	public void testMerge_empty_empty() {
+		
+	}
+	
+	public void testMerge_empty_nonempty() {
+		
+	}
+	
+	public void testMerge_nonempty_empty() {
+	
+	}
+	
+	public void testMerge_nonempty_nonempty() {
+		
+	}
+	
+	public void testMerge_withRedundancy() {
+		
+	}
+	
+	public void testDiff_empty_empty() {
+		
+	}
+	
+	public void testDiff_empty_nonempty() {
+		
+	}
+	
+	public void testDiff_nonempty_empty() {
+		
+	}
+	
+	public void testDiff_nonempty_nonempty() {
+		
+	}
+	
+	public void testDiff_withRedundancy() {
+		
+	}
+	@Test
+	
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+
+}
