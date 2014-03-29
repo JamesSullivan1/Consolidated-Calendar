221a2495986591789fd0f8caf5140aa1ff8d22e4
DanWehr <danwehr@gmail.com>
2014-03-28 20:13:30 -0600

    Added main() method to test suite to allow for making the JUnit tests into a jar file.

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -8,5 +9,9 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ TestCalendar.class, TestEvent.class, TestICSDownload.class,
 		TestParser.class })
 public class AllTests {
+
+	public static void main(String[] args) throws Exception {
+		JUnitCore.main("test.AllTests");
+	}
 
 }
